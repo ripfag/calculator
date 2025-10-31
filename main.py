a14a7665d59db7cff1de6fe7d37d4948782c233e
ripfag <aleksandrbistrov1@bk.ru>
2025-10-31 20:27:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-from sys import excepthook
-
-try:
-    num1 = int(input("Введите первое число:  "))
-    num2 = int(input("Введите второе число:  "))
-except ValueError:
-    print("Вы ввели не число, введите число!")
-
 message = '''
 выберите операцию
 + : сложение        
@@ -15,22 +7,36 @@ message = '''
 вы выбрали:
 '''
 
-operation = input(message)
-if operation == '+':
-    print('сложение')
-    result = num1 + num2
-elif operation == '-':
-    print('вычитание')
-    result = num1 - num2
-elif operation == '*':
-    print('умножение')
-    result = num1 * num2
-elif operation == '/':
-    print('деление')
-    try:
+try:
+    num1 = int(input("Введите первое число:  "))
+    num2 = int(input("Введите второе число:  "))
+    operation = input(message)
+
+    if operation == '+':
+        print('сложение')
+        result = num1 + num2
+
+    elif operation == '-':
+        print('вычитание')
+        result = num1 - num2
+
+    elif operation == '*':
+        print('умножение')
+        result = num1 * num2
+
+    elif operation == '/':
+        print('деление')
         result = num1 / num2
-    except ZeroDivisionError:
-        result = "Делить на 0 нельзя!"
+
+except ValueError:
+    result = "Вы ввели не число, введите число!"
+
+except NameError:
+    result = "Вы ввели не число, введите число!!"
+
+except ZeroDivisionError:
+    result = "Делить на 0 нельзя!"
+
 else:
     result = "Неизвестная операция"
 print("результат: ", result)
